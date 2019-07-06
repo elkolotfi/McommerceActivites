6d4ab50d762042cc24126ed1ce184830a3238c68
elkolotfi <elkolotfi@gmail.com>
2019-07-06 14:27:18 +0200

    get expedition by id | closes #7

--- mexpedition/src/main/java/com/mcommerce/microserviceexpedition/web/controller/ExpeditionController.java
+++ mexpedition/src/main/java/com/mcommerce/microserviceexpedition/web/controller/ExpeditionController.java
@@ -2,21 +2,32 @@ package com.mcommerce.microserviceexpedition.web.controller;
 
 import com.mcommerce.microserviceexpedition.dao.ExpeditionDao;
 import com.mcommerce.microserviceexpedition.model.Expedition;
+import com.mcommerce.microserviceexpedition.web.exception.ExpeditionNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Optional;
 
 @Controller @AllArgsConstructor
 @RequestMapping("/Expeditions")
 public class ExpeditionController {
     private final ExpeditionDao expeditionDao;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expedition> ExpeditionById(@PathVariable int id) {
+        Optional<Expedition> expedition = expeditionDao.findById(id);
+
+        if(!expedition.isPresent())
+            throw new ExpeditionNotFoundException(String.format("Expedition with id: {} was not found", id));
+
+        return new ResponseEntity<Expedition>(expedition.get(), HttpStatus.OK);
+    }
 
     @PostMapping("")
     public ResponseEntity<Expedition> createExpedition(@Valid @RequestBody Expedition expedition) {
---
+++ mexpedition/src/main/java/com/mcommerce/microserviceexpedition/web/exception/ExpeditionNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mcommerce.microserviceexpedition.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ExpeditionNotFoundException extends RuntimeException {
+    public ExpeditionNotFoundException(String message) {
+        super(message);
+    }
+}
